20574755421655b27848f6ede566e62e73d92107
quanglongle913 <quanglongle913@gmail.com>
2023-04-15 08:01:26 +0700

    done

--- RaycastExp/Assets/Scripts/RayCastDemo.cs
+++ RaycastExp/Assets/Scripts/RayCastDemo.cs
@@ -1,21 +1,23 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RayCastDemo : MonoBehaviour
 {
 
-    [SerializeField]private GameObject gameObject;
+    [SerializeField]private GameObject ball;
+
+    
     // Start is called before the first frame update
     void Start()
     {
         RaycastHit[] hits;
-        if (gameObject == null)
+        if (ball == null)
         {
             return;
         }
         else {
-            hits = Physics.RaycastAll(gameObject.transform.position, gameObject.transform.right, 100.0F);
+            hits = Physics.RaycastAll(ball.transform.position, ball.transform.right, 100.0F);
 
        
             for (int i = 0; i < hits.Length; i++)
@@ -25,12 +27,6 @@ public class RayCastDemo : MonoBehaviour
 
                 if (rend)
                 {
-                    // Change the material of all hit colliders
-                    // to use a transparent shader.
-                    rend.material.shader = Shader.Find("Transparent/Diffuse");
-                    Color tempColor = rend.material.color;
-                    tempColor.a = 0.3F;
-                    rend.material.color = tempColor;
                     Debug.Log("Did Hit" + hit.collider.name);
                 }
             }
@@ -40,6 +36,20 @@ public class RayCastDemo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("Ground")))
+            {
+                //khoảng cách từ gốc tòa độ đến điểm va chạm
+                float distance = hit.distance; 
+                //Tính tọa độ điểm va chạm: Gốc + Khoảng cách * Hướng của tia
+                Vector3 ObjPosition = ray.origin + distance * ray.direction;
+                ball.transform.position = new Vector3(ObjPosition.x, ObjPosition.y, ObjPosition.z);
+            }
+        }
+       
     }
 }
